9261ad23e159ce6a0b8e4ff0dd9dc10721eae67d
alexpavlov234 <78794923+alexpavlov234@users.noreply.github.com>
2022-07-15 16:47:27 +0300

    The date and region check has been optimised and a method has been created to check the last digit of the unique citizenship number.

--- UCNChecker/Program.cs
+++ UCNChecker/Program.cs
@@ -2,41 +2,61 @@
 {
     internal class Program
     {
-        static Dictionary<string, string> _dictionary = ReadFromFile(AppDomain.CurrentDomain.BaseDirectory + "/Cities.txt");
+        static Dictionary<string, string> RegionsDictionary = ReadFromFile(AppDomain.CurrentDomain.BaseDirectory + "/Regions.txt");
         static void Main(string[] args)
         {
-
-
-            UcnCheck("9931136319");
+                    
         }
-        static bool UcnCheck(string UCN)
+        static bool UCNCheck(string UCN)
         {
             try
             {
                 if (UCN.Length != 10 || !IsDigitsOnly(UCN))
                 {
-                    //Console.WriteLine("dd");
                     return false;
                 }
                 else
                 {
                     string wholeDate = UCN.Substring(0, 6);
                     int monthDate = Int32.Parse(UCN.Substring(2, 2));
-                   
 
-                    if (monthDate <= 12 && IsValidDate(wholeDate))
+                    if (monthDate <= 12 && IsValidDate(WholeDateNormalizer(wholeDate, 0)))
                     {
 
+                        if (CheckCityRegion(UCN))
+                        {
+                            return LastNumChecker(UCN);
+                        }
+                        else
+                        {
+                            return false;
+                        }
                         return true;
                     }
                     else if (monthDate > 12 && monthDate <= 32 && IsValidDate(WholeDateNormalizer(wholeDate, 20)))
                     {
 
+                        if (CheckCityRegion(UCN))
+                        {
+                            return LastNumChecker(UCN);
+                        }
+                        else
+                        {
+                            return false;
+                        }
                         return true;
                     }
                     else if (monthDate > 40 && monthDate <= 52 && IsValidDate(WholeDateNormalizer(wholeDate, 40)))
                     {
 
+                        if (CheckCityRegion(UCN))
+                        {
+                                return LastNumChecker(UCN);
+                            }
+                        else
+                        {
+                            return false;
+                        }
                         return true;
                     }
                     else
@@ -51,7 +71,6 @@
                 return false;
             }
         }
-
         static bool IsDigitsOnly(string str)
         {
             foreach (char c in str)
@@ -64,17 +83,36 @@
         }
         static bool IsValidDate(string str)
         {
-            if (DateTime.TryParseExact(str, "yyMMdd", null, System.Globalization.DateTimeStyles.AllowWhiteSpaces |
-                               System.Globalization.DateTimeStyles.AdjustToUniversal, out DateTime result))
-                return false;
-            else
+            if (DateTime.TryParseExact(str, "yyyyMMdd", null, System.Globalization.DateTimeStyles.AllowWhiteSpaces | System.Globalization.DateTimeStyles.AdjustToUniversal, out DateTime result))
                 return true;
-
+            else
+                return false;
+        }
+        static string FormatDate(string str)
+        {
+            if (DateTime.TryParseExact(str, "yyyyMMdd", null, System.Globalization.DateTimeStyles.AllowWhiteSpaces | System.Globalization.DateTimeStyles.AdjustToUniversal, out DateTime result))
+                return result.ToString("dd-MM-yyyy");
+            else
+                return null;
         }
 
         static string WholeDateNormalizer(string date, int number)
         {
-            date = date.Substring(0, 2) + string.Format("{0:00}", Int32.Parse(date.Substring(2, 2)) - number) + date.Substring(2 + 2);
+            if (number == 0)
+            {
+                date = "19" + date.Substring(0, 2) + string.Format("{0:00}", Int32.Parse(date.Substring(2, 2)) - number) + date.Substring(4);
+            }
+            else if (number == 20)
+            {
+                date = "18" + date.Substring(0, 2) + string.Format("{0:00}", Int32.Parse(date.Substring(2, 2)) - number) + date.Substring(4);
+            }
+            else if (number == 40)
+            {
+                date = "20" + date.Substring(0, 2) + string.Format("{0:00}", Int32.Parse(date.Substring(2, 2)) - number) + date.Substring(4);
+            }
+
+
+
             return date;
         }
 
@@ -82,11 +120,60 @@
         {
             return File.ReadLines(file).ToDictionary(x => x.Split(',')[0].Trim(), x => x.Split(',')[1].Trim());
         }
-        //       public bool IsValidDate(int year, int month, int day)
-        //{
-        //    return year >= 1 && year <= 9999
-        //            && month >= 1 && month <= 12
-        //            && day >= 1 && day <= DateTime.DaysInMonth(year, month);
-        //}
+
+        static bool CheckCityRegion(string UCN)
+        {
+            return RegionsDictionary.ContainsKey(UCN.Substring(6, 3));
+        }
+
+        static bool LastNumChecker(string UCN)
+        {
+            int sum = 0;
+            int divisionWithRemainder;
+            int lastNumberOfUCN;
+
+            char[] numbersCharArray = UCN.ToCharArray();
+
+            int[] numbersIntegerArray = Array.ConvertAll(numbersCharArray, c => (int)Char.GetNumericValue(c));
+
+            int[] numbersWeight = { 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+
+
+            for (int i = 0; i < numbersWeight.Length; i++)
+            {
+                sum += numbersIntegerArray[i] * numbersWeight[i];
+            }
+
+            divisionWithRemainder = sum / 11;
+
+            lastNumberOfUCN = sum - 11 * divisionWithRemainder;
+
+            if (lastNumberOfUCN < 10)
+            {
+
+                if (UCN.Substring(UCN.Length - 1).Equals(lastNumberOfUCN.ToString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                lastNumberOfUCN = 0;
+                if (UCN.Substring(UCN.Length - 1).Equals(lastNumberOfUCN.ToString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
     }
+
 }
